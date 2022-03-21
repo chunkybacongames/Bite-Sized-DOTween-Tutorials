f4f39ce2a2539b56ed7ad11a65b374d4b72bf9cf
Sami Ahlfors <48211518+samiahlfors@users.noreply.github.com>
2022-03-21 17:54:33 +0100

    Create Move.cs

---
+++ Move.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class Move : MonoBehaviour
+{
+    private void Start()
+    {
+        OnMoveXAxis();
+        //OnMoveX();
+    }
+    
+    private void OnMoveXAxis()
+    {
+        transform.DOMoveX(5.0f, 2.0f)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.InOutSine);
+    }
+    
+    private void OnMoveX()
+    {
+        Vector3 position = new Vector3(5.0f, 4.0f, 3.0f);
+        transform.DOMove(position, 2.0f)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.InOutSine);
+    }
+}
